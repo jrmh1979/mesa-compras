a3dca7b60832659255a77f0431efd08caa18c4b1
jrmh1979 <jrmartinezh5@hotmail.com>
2025-05-30 14:37:29 -0500

    Corrige CORS solo para Vercel

--- app.js
+++ app.js
@@ -13,17 +13,14 @@ const port = process.env.PORT || 5000;
 
 // ✅ Configurar CORS antes de rutas
 const corsOptions = {
-  origin: [
-    'http://localhost:3001',
-    'http://localhost:3002',
-    'https://mesa-compras-frontend.vercel.app' // 👈 este es el correcto
-  ],
+  origin: 'https://mesa-compras-frontend.vercel.app',
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
 };
 
 app.use(cors(corsOptions));
+app.options('*', cors(corsOptions)); // preflight
 
 
 // Middleware
